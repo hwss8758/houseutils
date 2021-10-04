18b60d9650c48405c1a47712d96ce423eccf38e0
wonsang.hwang <wonsang.hwang@navercorp.com>
2021-10-04 11:19:46 +0900

    health check controller 생성

---
+++ src/main/kotlin/com/example/houseutils/HouseutilsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.houseutils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseutilsApplication
+
+fun main(args: Array<String>) {
+    runApplication<HouseutilsApplication>(*args)
+}
